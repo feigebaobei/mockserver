c0016e3765e40babbcbadb30621e54e33ab9a710
feigebaobei <18515195415@163.com>
2020-05-05 20:59:03 +0800

    使用9876端口

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 允许跨域
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Max-Age', 3600)
+  next()
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
