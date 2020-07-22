b3cb8cbfced9a13a93c0dcf3362f7f929e8b62af
feigebaobei <18515195415@163.com>
2020-07-22 16:52:42 +0800

    把本地可运行的webSocket.js推到灰度服务器上

--- routes/webSocket.js
+++ routes/webSocket.js
@@ -8,49 +8,67 @@ const Base64 = require('js-base64').Base64
 var redis = require('redis')
 var config = require('../lib/config')
 var bodyParse = require('body-parser')
+
 const WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({
+    // host: '127.0.0.1', // ok
+    // host: 'lixiaodan.org',
+    // host: 'lixiaodan',
+    // host: '47.94.105.206',
+    // host: '172.25.0.51',
+    // server: 'lixiaodan.org',
     port: config.webSocket.port || 9870
   })
+// console.log('wss', wss)
+wss.on('connection', (ws) => {
+  // 参数ws是一个websocket的实例
+  // console.log('服务端：客户端已经连接', ws)
+  console.log('服务端：客户端已经连接')
+  ws.on('message', (message) => {
+    console.log(message)
+    ws.send(`receive: ${message}`)
+  })
+})
 
-// router.use(bodyParse.json())
-// let red_config = config.redis,
-//     RED_HOST = red_config.host,
-//     RED_PWD = red_config.pass,
-//     RED_PORT = red_config.port,
-//     RED_OPTS = {auth_pass: RED_PWD},
-//     client = redis.createClient(RED_PORT, RED_HOST, RED_OPTS)
-// client.on('ready', (res) => {
-//   console.log('ready')
+// const WebSocket = require('ws')
+// // const ws = new WebSocket('ws://www.host.com/path')
+// const ws = new WebSocket('wss://echo.websocket.org/', {
+//   origin: 'https://websocket.org'
 // })
-// client.on('end', (res) => {
-//   console.log('end')
+// ws.on('open', function open() {
+//   console.log('connected');
+//   ws.send(Date.now());
+// });
+// ws.on('close', function close() {
+//   console.log('disconnected');
+// });
+// ws.on('message', function incoming(data) {
+//   console.log(`Roundtrip time: ${Date.now() - data} ms`);
+//   setTimeout(function timeout() {
+//     ws.send(Date.now());
+//   }, 500);
+// });
+
+// ws.on('open', function () {
+//   ws.send('something open')
 // })
-// client.on('error', (error) => {
-//   console.log('error', error)
-// })
-// client.on('connect', (res) => {
-//   console.log('connect')
+// ws.on('message', function (data) {
+//   console.log(data)
+//   ws.send(`receive: ${data}`)
 // })
 
-var speed = {
-  'A1111': 95.0,
-  'A2222': 50.0
-}
+
+
+// var speed = {
+//   'A1111': 95.0,
+//   'A2222': 50.0
+// }
 // var randomSpeedUpdater = function () {
 //     for (var item in speed) {
 //         var randomizedChange = Math.random(60, 120);
 //         speed[item] += randomizedChange;
 //     }
 // }
-
-wss.on('connection', (ws) => {
-  // console.log('服务端：客户端已经连接', ws)
-  console.log('服务端：客户端已经连接')
-  ws.on('message', (message) => {
-    console.log(message)
-  })
-})
 
 // /* GET users listing. */
 // router.route('/test')
@@ -70,4 +88,4 @@ wss.on('connection', (ws) => {
 //     res.send('delete')
 //   })
 
-module.exports = router;
+// module.exports = router;
